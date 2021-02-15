07e9a6e5fed9826d3ac9e26de68a88c4ec88a660
miloser <miloseric@gmail.com>
2021-02-15 03:04:36 +0100

    PA2 upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Using the same code as in makeVector function, with new variable name
+  inve <- NULL
+  set <- function(y) {
+    x <<- y
+    inve <<- NULL
+  }
+  get <- function() x
+  ## This is where our function differs, instead of a 
+  ## mean we're looking for an inverse matrix
+  setinve <- function(inverse) inve <<- inverse
+  getinve <- function() inve
+  list(set = set, get = get,
+       setinve = setinve,
+       getinve = getinve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inve <- x$getinve()
+  if(!is.null(inve)) {
+    message("getting cached data")
+    return(inve)
+  }
+  datainve <- x$get()
+  ##It took a lot of time to work out the next line :)
+  inve <- solve(datainve, ...)
+  x$setinv(inve)
+  inve
+  ## Return a matrix that is the inverse of 'x'
 }
